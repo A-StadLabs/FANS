b9aba65b2096799c3ced2ecf81f4c2e7f6dce644
kingflurkel <mikethuy@gmail.com>
2014-10-08 17:08:43 +0200

    test

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,6 +6,12 @@
     console.log('Polymer is ready to rock!');
   });
 
+  document.addEventListener('route', function(e, detail, sender) {
+    // Perform some behaviour
+    console.log('route', e.detail.route);
+    router.go(e.detail.route);
+  });
+
 
 // wrap document so it plays nice with other libraries
 // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
