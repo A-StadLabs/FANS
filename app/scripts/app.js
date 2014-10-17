975e6f60d293e450905b984e99817111d3a424cf
kingflurkel <mikethuy@gmail.com>
2014-10-17 16:17:58 +0200

    yooow

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,15 @@
 (function(document) {
   'use strict';
 
+  var elm = document.body; // or some selection of the element you want to disable
+
+var catcher = function(evt) {
+    if (evt.touches.length < 2)
+        evt.preventDefault();
+};
+
+elm.addEventListener('touchstart', catcher, true);
+
   document.addEventListener('polymer-ready', function() {
     // Perform some behaviour
     console.log('Polymer is ready to rock!');
